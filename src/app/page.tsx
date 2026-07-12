70f2b19c47dbd6cf0cfa01d2d8799ebf1ce4c727
Ashfaq <ashrahman777@gmail.com>
2026-07-13 02:33:20 +0530

    fix background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,8 @@ export default async function Home() {
     .order('created_at', { ascending: false })
 
   return (
-    <div className="min-h-screen bg-[#fafafa] bg-[radial-gradient(#cbd5e1_2px,transparent_2px)] [background-size:24px_24px] text-gray-900 font-sans flex flex-col">
-      {/* 
-        The Navbar has been moved inside BookmarkList so it can 
-        easily control the mobile sidebar menu state! 
-      */}
+    // <div className="min-h-screen bg-[#fafafa] bg-[radial-gradient(#cbd5e1_2px,transparent_2px)] [background-size:24px_24px] text-gray-900 font-sans flex flex-col">
+      <div className="min-h-screen bg-[url('/background.jpeg')] bg-cover bg-center bg-fixed text-gray-900 font-sans flex flex-col">
       <BookmarkList 
         initialBookmarks={bookmarks || []} 
         userEmail={user.email} 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -74,7 +74,7 @@ export default function Sidebar({
       )}
 
       <aside className={`
-        fixed inset-y-0 left-0 z-50 w-[280px] bg-[#fafafa] border-r-4 border-gray-900 p-6 overflow-y-auto transition-transform duration-300 ease-in-out
+        fixed inset-y-0 left-0 z-50 w-[350px] bg-[#fafafa] border-r-4 border-gray-900 p-6 overflow-y-auto transition-transform duration-300 ease-in-out
         ${isMobileMenuOpen ? 'translate-x-0 shadow-[8px_0px_0px_0px_rgba(17,24,39,1)]' : '-translate-x-full'}
         md:sticky md:translate-x-0 md:w-64 md:top-28 md:self-start md:h-[calc(100vh-8rem)] md:p-0 md:border-none md:bg-transparent md:shadow-none md:z-0
         flex flex-col space-y-6
