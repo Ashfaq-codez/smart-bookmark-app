1c9103624867b4f3181b442d38d8cb4c7e98c146
Ashfaq <ashrahman777@gmail.com>
2026-07-13 03:21:12 +0530

    change background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
 
   return (
     // <div className="min-h-screen bg-[#fafafa] bg-[radial-gradient(#cbd5e1_2px,transparent_2px)] [background-size:24px_24px] text-gray-900 font-sans flex flex-col">
-      <div className="min-h-screen bg-[url('/background.jpeg')] bg-cover bg-center bg-fixed text-gray-900 font-sans flex flex-col">
+      <div className="min-h-screen bg-[url('/background.gif')] bg-cover bg-center bg-fixed text-gray-900 font-sans flex flex-col">
       <BookmarkList 
         initialBookmarks={bookmarks || []} 
         userEmail={user.email} 
